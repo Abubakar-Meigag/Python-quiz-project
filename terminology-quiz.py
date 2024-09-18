2efbd1d908380ec4712e7783556ae95323f20e05
Beko Meigag <bekomeigag@cyfs-MBP.communityfibre.co.uk>
2024-09-18 11:27:30 +0100

    done with all the quiz questions

---
+++ terminology-quiz.py
@@ -0,0 +1,42 @@
+import random
+
+print("Welcome to AWS re/Start Terminology quiz")
+
+player = input('Do you want to play')
+
+if player.lower() != 'yes':
+      quit()
+
+print("Okay! Let's play :)")
+
+
+quiz_questions = [
+    ("What does CPU stand for?", "central processing unit"),
+    ("What does GPU stand for?", "graphics processing unit"),
+    ("What does RAM stand for?", "random access memory"),
+    ("What does SSD stand for?", "solid state drive"),
+    ("What does Nmap stand for?", "network mapper"),
+    ("What does ICMP stand for?", "internet control message protocol"),
+    ("What does SNMP stand for?", "simple network management protocol"),
+    ("What does NAT stand for?", "network address translation"),
+    ("What does IPS stand for?", "intrusion prevention system"),
+    ("What does CIDR stand for?", "classless inter-domain"),
+    ("What does NIC stand for?", "network interface card "),
+    ("What does AWS stand for?", "amazon web services"),
+    ("What does RDS in AWS  stand for?", "relational database service"),
+    ("What does S3  in AWS  stand for?", "basic input output system"),
+    ("What does EC2 in AWS  stand for?", "elastic compute cloud"),
+    ("What does ECS in AWS  stand for?", "elastic container service"),
+    ("What does EKS in AWS  stand for?", "elastic kubernetes service"),
+    ("What does VPC in AWS  stand for?", "virtual private cloud"),
+    ("What does ELB in AWS  stand for?", "elastic load balancing"),
+    ("What does IAM in AWS  stand for?", "identity and access management"),
+    ("What does SNS in AWS  stand for?", "simple notification service"),
+    ("What does ELB in AWS  stand for?", "elastic load balancing"),
+    ("What does Route 53 in AWS  stand for?", "dns web service"),
+    ("What does KMS in AWS  stand for?", "key management service"),
+    ("What does DynamoDB in AWS  stand for?", "noSQL database service"),
+    ("What does CloudWatch in AWS  stand for?", "monitoring and logging service"),
+    ("What does HTTP stand for?", "hypertext transfer protocol"),
+    ("What does HDD stand for?", "hard disk drive")
+]
